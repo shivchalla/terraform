5d54b0a76901561ae26005bcfb06fbac5c58f240
shivchalla <161644518+shivchalla@users.noreply.github.com>
2025-12-19 23:33:29 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,15 +1,8 @@
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.42.0"
+      version = "~> 4.42.0"
     }
   }
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-  subscription_id = "2fe27ce6-1055-460c-8546-9b26931a4b9a"
 }
